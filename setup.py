a2b659c4420fcdecf64eb3753358f7a8c34bbd68
Thor Kooda <tkooda@github>
2015-04-04 17:19:47 -0500

    increment version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 setup( name = 'namecoin_tinydns_data',
-       version = '0.2',
+       version = '0.3',
        scripts = [ "bin/namecoin-tinydns-data" ]
        )
 
